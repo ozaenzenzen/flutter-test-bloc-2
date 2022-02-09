eed4cbdc05696d994bd62fcc95874f5b1068226c
ozaenzenzen <recovery252@gmail.com>
2022-02-09 22:37:23 +0700

    update counter state

--- lib/bloc/counter_bloc/counter_state.dart
+++ lib/bloc/counter_bloc/counter_state.dart
@@ -4,3 +4,13 @@ part of 'counter_bloc.dart';
 abstract class CounterState {}
 
 class CounterInitial extends CounterState {}
+
+class CounterLoading extends CounterState {}
+
+class CounterCalculated extends CounterState {}
+
+class CounterError extends CounterState {
+  final String errorMessage;
+
+  CounterError(this.errorMessage);
+}
