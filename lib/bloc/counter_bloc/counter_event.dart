c23620f3886e8d3ca2f42be3b7bf4259d3643259
ozaenzenzen <recovery252@gmail.com>
2022-02-11 20:43:28 +0700

    update counter event

--- lib/bloc/counter_bloc/counter_event.dart
+++ lib/bloc/counter_bloc/counter_event.dart
@@ -3,15 +3,19 @@ part of 'counter_bloc.dart';
 @immutable
 abstract class CounterEvent {}
 
-class CounterCalculation extends CounterEvent {
+class CounterIncrement extends CounterEvent {
   final double numIncrement1;
   final double numIncrement2;
 
-  CounterCalculation(this.numIncrement1, this.numIncrement2);
+  CounterIncrement(this.numIncrement1, this.numIncrement2);
 }
 
-// class CounterDecrement extends CounterEvent {
-//   final double numDecrement;
+class CounterDecrement extends CounterEvent {
+  final double numDecrement1;
+  final double numDecrement2;
 
-//   CounterDecrement(this.numDecrement);
-// }
+  CounterDecrement(
+    this.numDecrement1,
+    this.numDecrement2,
+  );
+}
