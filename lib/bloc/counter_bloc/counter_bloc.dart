d31c086b5f43c66b75b309c22662a2bc2227d4f0
ozaenzenzen <recovery252@gmail.com>
2022-02-11 20:41:32 +0700

    update counter bloc

--- lib/bloc/counter_bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc/counter_bloc.dart
@@ -1,18 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:test_bloc_2/services/counter_services.dart';
 
 part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterServices? counterServices;
   CounterBloc() : super(CounterInitial()) {
     on<CounterEvent>((event, emit) {
       // TODO: implement event handler
     });
-    on<CounterIncrement>((event, emit) {
+    on<CounterCalculation>((event, emit) {
       emit(CounterLoading());
-      try {
-        emit(CounterCalculated(event.numIncrement));
+      try {                
+        emit(CounterCalculated(counterServices!.incrementFunction(event.numIncrement1, event.numIncrement2)));
       } catch (e) {
         emit(CounterError(e.toString()));
       }
