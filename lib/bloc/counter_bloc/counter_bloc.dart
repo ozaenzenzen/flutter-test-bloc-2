b15a02794f8b6f03ba9c92154ee75f339282815e
ozaenzenzen <recovery252@gmail.com>
2022-02-14 08:09:40 +0700

    update counter bloc

--- lib/bloc/counter_bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc/counter_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:test_bloc_2/services/counter_services.dart';
 
@@ -12,11 +13,20 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
       // TODO: implement event handler
     });
     on<CounterIncrement>((event, emit) {
+      debugPrint("dapet angka berapa? ${event.numIncrement1} ${event.numIncrement2}");
       emit(CounterLoading());
+
       try {
-        emit(CounterCalculated(
-          counterServices!.incrementFunction(event.numIncrement1, event.numIncrement2),
-        ));
+        // var hasil = counterServices!.incrementFunction(
+        //   event.numIncrement1.toDouble(),
+        //   event.numIncrement2.toDouble(),
+        // );
+
+        double hasil = event.numIncrement1.toDouble() + event.numIncrement2.toDouble();
+
+        // var hasil = 20.toDouble();
+        // debugPrint("hasil: $hasil");
+        emit(CounterCalculated(hasil));
       } catch (e) {
         emit(CounterError(e.toString()));
       }
@@ -25,7 +35,7 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
       emit(CounterLoading());
       try {
         emit(CounterCalculated(
-          counterServices!.incrementFunction(event.numDecrement1, event.numDecrement2),
+          counterServices!.incrementFunction(event.numDecrement1.toDouble(), event.numDecrement2.toDouble()),
         ));
       } catch (e) {
         emit(CounterError(e.toString()));
